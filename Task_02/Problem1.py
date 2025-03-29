a75a477920ea205e28a3c354a09c0864b812b8e8
Jana <janahassan2052004@gmail.com>
2025-03-29 21:03:09 +0000

    Get Smallest and Largest Number

---
+++ Task_02/Problem1.py
@@ -0,0 +1,12 @@
+Nums= []
+while True:
+   Num = int(input())
+   if Num == -1:
+    break
+   Nums.append(Num)
+if Nums:
+   print(max(Nums), min(Nums))
+else:
+    print("No valid numbers entered.")
+
+
