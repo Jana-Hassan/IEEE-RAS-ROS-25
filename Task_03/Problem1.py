caa150ee74e69084c0fb82644b3b632a06b4a75c
Jana <janahassan2052004@gmail.com>
2025-04-11 07:50:17 +0000

    Library Dic

---
+++ Task_03/Problem1.py
@@ -0,0 +1,19 @@
+library = {
+    "Book 01": {
+        "title": "The Setting Sun",
+        "author": "Dazai Osamu",
+        "year": 1947
+    },
+    "Book 02": {
+        "title": "Heaven",
+        "author": "Mieko Kawakami",
+        "year": 2009
+    },
+    "Book 03": {
+        "title": "Golden Sun",
+        "author": "Pierce Brown",
+        "year": 2015
+    }
+}
+
+print(library)
